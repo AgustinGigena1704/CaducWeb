b6aee47115165905898d458ddd19f2e2c808368c
AgustinGigena1704 <agustingigena1704@gmail.com>
2023-07-03 23:53:24 -0300

    Reorganizacion

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+function cambio(Tipo){
+    console.log(Tipo);
+    if(Tipo == "Actividades"){
+        window.location.href = "Actividades.html";
+    }
+    else if(Tipo == "Empresas"){
+        window.location.href = "Empresas.html";
+    }
+    else if(Tipo == "Grupos"){
+        window.location.href = "Grupos.html";
+    }
+    else if(Tipo == "Gubernamentales"){
+        window.location.href = "Gubernamentales.html";
+    }
+}
